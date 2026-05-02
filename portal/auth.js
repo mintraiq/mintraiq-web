62dc1680a44fcd7d89552927d40629d1aeb3024c
rambalaji <heyramji@gmail.com>
2026-05-02 17:01:23 +1200

    updated the auth.js by removing the version for logto

--- portal/auth.js
+++ portal/auth.js
@@ -3,7 +3,7 @@
  * Use jsDelivr `+esm` instead of esm.sh to avoid 404 / edge issues on Vercel and other CDNs.
  * @see https://www.jsdelivr.com/package/npm/@logto/browser
  */
-import LogtoClient from 'https://cdn.jsdelivr.net/npm/@logto/browser@4.1.7/+esm';
-
+//import LogtoClient from 'https://cdn.jsdelivr.net/npm/@logto/browser@4.1.7/+esm';
+import LogtoClient from 'https://esm.sh/@logto/browser';
 export { LogtoClient };
 export default LogtoClient;
